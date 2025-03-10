f3f71429a4cb209a641d5ffc2f5e8afcaee8c1df
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2025-03-11 03:11:22 +0530

    added terminal in server with socket.io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
-const http = require('http');
-const express = require('express');
-const {Server:SocketServer}= require('socket.io');
+import os from "os";
+import http from "http";
+import express from "express";
+import {Server as SocketServer} from "socket.io";
+import pty from "node-pty";
+var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+const ptyProcess = pty.spawn(shell, [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    cwd: process.env.INIT_CWD,
+    env: process.env
+  });
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketServer({
     cors: '*'
+})
+
+io.attach(server);
+
+ptyProcess.onData((data) => {
+    io.emit('terminal:data',data);
+  });
+
+io.on('connection',(socket)=>{
+    console.log(`Socket Connected`, socket.id);
+
+    socket.on('terminal:write',(data)=>{
+        ptyProcess.write(data);
+    })
+})
+
+server.listen(9000,()=>{
+    console.log(`🐋Docker server is running on port ${9000}`);
 })
